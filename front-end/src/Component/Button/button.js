f264c9e491685a6a3d33f15b1a6abdadc8a9f86e
David <david123rana@gmail.com>
2023-06-03 13:51:12 +0530

    updated

--- front-end/src/Component/Button/button.js
+++ front-end/src/Component/Button/button.js
@@ -4,26 +4,36 @@ import Button from '@mui/material/Button';
 import { useHistory } from "react-router-dom"
 import OldNavBar from '../../Component/NavBar';
 
-const divStyle = {margin: 'auto', position:'absolute', top:'30%', left:'30%'}
+const divStyle = {margin: 'auto', position:'absolute', top:'20%', left:'25%'}
 const btnstyle = {width:'10rem' , height:'4rem', borderRadius: '10px',}
 
 function ButtonCom() {
     const history = useHistory();
-
-    const oldVersionReturn = () => {
-        return (
-            <>
-                <OldNavBar />
-            </>
-        )
+    const navigateFunction = (location) => {
+        if(location === 'create-record'){
+            history.push('/add')
+        }
+        if(location === 'created-list'){
+            history.push('/addRecords')
+        }
+        if(location === 'add-item'){
+            history.push('/result')
+        }
+        if(location === 'track-item'){
+            history.push('/list')
+        }
+        if(location === 'old-version'){
+            history.push('/oldversion1')
+        }
     }
   return (
         
             <Stack  direction="row" spacing={2}  style={divStyle}>
-            <Button variant="contained" color='secondary' style={btnstyle} onClick={() => history.push('/add')} >Add Item</Button>
-        <Button variant="contained" color='secondary' style={btnstyle}  >Add Loaction</Button>
-        <Button variant="contained" color='secondary' style={btnstyle}>Place Item</Button>
-        <Button variant="contained" color='secondary' style={btnstyle} onClick={() => history.push('/oldversion1')}>Old Version</Button>
+            <Button variant="contained" color='secondary' style={btnstyle} onClick={() => navigateFunction('create-record')} >Create Record</Button>
+            <Button variant="contained" color='secondary' style={btnstyle} onClick={() => navigateFunction('created-list')}>Created List</Button>
+        <Button variant="contained" color='secondary' style={btnstyle} onClick={() => navigateFunction('add-item')}>Add Item</Button>
+        <Button variant="contained" color='secondary' style={btnstyle} onClick={() => navigateFunction('track-item')}>Track Item</Button>
+        <Button variant="contained" color='secondary' style={btnstyle} onClick={() => navigateFunction('old-version')}>Old Version</Button>
             </Stack>
         
   )
--- front-end/src/Page/Home/index.js
+++ front-end/src/Page/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Btn from '../../Component/Button/button';
+import Tab from '../../Component/Tab/tab';
 
 function index() {
   return (
     <>
-    <Btn />
+    <Tab />
+    {/* <Btn /> */}
     </>
   )
 }
